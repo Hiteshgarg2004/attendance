b5cb14d183f48769e73361344ad98d646421e756
Hiteshgarg2004 <146863298+Hiteshgarg2004@users.noreply.github.com>
2026-05-28 14:52:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["https://attendance-fw8v.onrender.com"],
     credentials: true,
   })
 );
