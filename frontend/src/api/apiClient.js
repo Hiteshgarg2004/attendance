1ea4fb90b7d44793f782b924e644b23baf4634d1
Hiteshgarg2004 <146863298+Hiteshgarg2004@users.noreply.github.com>
2026-05-29 00:31:41 +0530

    Refactor token handling in API request interceptor
    Refactor token attachment logic for clarity.

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -1,13 +1,18 @@
+
+
 import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://attendance-fw8v.onrender.com",
 });
 
-// Automatically attach token from localStorage
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
   return config;
 });
 
